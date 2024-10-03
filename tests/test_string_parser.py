9fe4b3e67b9838ad390f98c18d00f708441d980b
Matteo Greco <matteo@hlpy.co>
2024-10-03 18:50:59 +0200

    add string parser and test for string parser

---
+++ tests/test_string_parser.py
@@ -0,0 +1,44 @@
+import sys
+from unittest.case import TestCase
+sys.path.append('src')
+from lib.string_parser import extract_price, extract_number, extract_imported, extract_product_name
+
+
+class TestStringParser(TestCase):
+
+    def test_extract_number(self):
+        string = "1 imported box of chocolates at 10.00"
+        extracted = extract_number(string)
+        self.assertEqual(extracted, 1)
+
+    def test_extract_number_fail(self):
+        string = "imported box of chocolates at 10.00"
+        self.assertRaises(IndexError, extract_number, string)
+
+    def test_imported(self):
+        string = "1 imported box of chocolates at 10.00"
+        extracted = extract_imported(string)
+        self.assertEqual(extracted, True)
+
+    def test_imported_fail(self):
+        string = "box of chocolates at 10.00"
+        self.assertRaises(IndexError, extract_imported, string)
+
+
+    def test_price(self):
+        string = "1 price box of chocolates at 10.00"
+        extracted = extract_price(string)
+        self.assertEqual(extracted, 10.00)
+
+    def test_price_fail(self):
+        string = "box of chocolates at"
+        self.assertRaises(IndexError, extract_price, string)
+
+    def test_product_name(self):
+        string = "1 imported box of chocolates at 10.00"
+        extracted = extract_product_name(string)
+        self.assertEqual(extracted, 'box of chocolates')
+
+    def test_product_name_fail(self):
+        string = "1 imported box of chocolates at 10.00"
+        self.assertRaises(IndexError, extract_product_name, string)
